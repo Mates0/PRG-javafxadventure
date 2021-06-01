a5d25d96aeb7eb455e4d48d9a5271a0d12b80c30
Mates0 <matyas.vanko@educanet.cz>
2021-06-01 13:46:08 +0200

    second commit

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -3,28 +3,19 @@ package gui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 public class Controller {
     @FXML
-    public Button btnCalculate;
+    public Button btn;
     @FXML
-    public TextField input1;
-    @FXML
-    public TextField input2;
-    @FXML
-    public ChoiceBox operator;
+    public Text text;
 
-    public void onCalculate(ActionEvent actionEvent) {
-
+    public void btnlevel2(ActionEvent actionEvent) {
+        text.setText("Level 2");
     }
 
-    public void onOperatorSelected(MouseEvent mouseEvent) {
-        operator.getItems().add("+");
-        operator.getItems().add("-");
-        operator.getItems().add("*");
-        operator.getItems().add("/");
+    public void btnlevel3(ActionEvent actionEvent) {
+        text.setText("Level 3");
     }
 }
