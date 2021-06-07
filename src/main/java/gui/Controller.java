9675136a4094f9d797104465955b59f9922e442a
Mates0 <matyas.vanko@educanet.cz>
2021-06-07 10:07:13 +0200

    third commit

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -3,6 +3,9 @@ package gui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 public class Controller {
@@ -11,19 +14,26 @@ public class Controller {
     @FXML
     public Button btnleft;
     @FXML
-    public Button btndown;
-    @FXML
     public Button btnright;
     @FXML
     public Text maintext;
+    @FXML
+    public Text subtext;
+    @FXML
+    public ImageView img;
 
     public void onBtnForward(ActionEvent actionEvent) {
+        maintext.setText("Machine Room");
+        subtext.setText("The Machine Room is a relatively narrow area, filled with pipes and machinery. This is a safe area, so Daniel won't encounter any enemy (the Shadow excluded).");
+        Image image = new Image("machine.png");
+        img.setImage(image);
     }
 
     public void onBtnLeft(ActionEvent actionEvent) {
-    }
-
-    public void onBtnDown(ActionEvent actionEvent) {
+        maintext.setText("Guest Room");
+        subtext.setText("The Guest Room is the room Daniel stayed in during his time at the Castle. It can be reached from the Back Hall.");
+        Image image = new Image("guest.jpg");
+        img.setImage(image);
     }
 
     public void onBtnRight(ActionEvent actionEvent) {
