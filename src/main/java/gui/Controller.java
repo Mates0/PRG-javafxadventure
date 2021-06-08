09857dce8e1034d682631c4a80645197b2557c50
Mates0 <matyas.vanko@educanet.cz>
2021-06-08 18:04:48 +0200

    final

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -2,12 +2,18 @@ package gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class Controller {
+import java.io.IOException;
+
+public class Controller extends Window {
     @FXML
     public Button btnforward;
     @FXML
@@ -20,9 +26,31 @@ public class Controller {
     public Text subtext;
     @FXML
     public ImageView img;
-    private int count = 0;
+    @FXML
+    public Button btnexit;
+    @FXML
+    public Text wintext;
+    private int count;
 
-    public void onBtnForward(ActionEvent actionEvent) {
+    public void onBtnForward(ActionEvent actionEvent) throws IOException {
+        if (count == 100) {
+            Stage stage = (Stage) btnleft.getScene().getWindow();
+            Parent root = FXMLLoader.load(
+                    getClass().getResource("/lose.fxml")
+            );
+            Scene scene = new Scene(root, 800, 600);
+            stage.setScene(scene);
+            stage.show();
+        }
+        if (count == -2) {
+            Stage stage = (Stage) btnleft.getScene().getWindow();
+            Parent root = FXMLLoader.load(
+                    getClass().getResource("/win.fxml")
+            );
+            Scene scene = new Scene(root, 800, 600);
+            stage.setScene(scene);
+            stage.show();
+        }
         if (count == -1) {
             maintext.setText("Machine Room");
             subtext.setText("The Machine Room is a relatively narrow area, filled with pipes and machinery. This is a safe area, so Daniel won't encounter any enemy.");
@@ -38,6 +66,7 @@ public class Controller {
             img.setImage(image);
             btnright.setDisable(true);
             btnleft.setDisable(true);
+            count = 100;
         }
         if (count == 1) {
             maintext.setText("Transept");
@@ -46,8 +75,8 @@ public class Controller {
             img.setImage(image);
             btnright.setDisable(true);
             btnleft.setDisable(true);
+            count = 100;
         }
-        System.out.println(count);
     }
 
     public void onBtnLeft(ActionEvent actionEvent) {
@@ -68,19 +97,9 @@ public class Controller {
             btnright.setDisable(false);
             count--;
         }
-        System.out.println(count);
     }
 
     public void onBtnRight(ActionEvent actionEvent) {
-        System.out.println(count);
-        if (count == -1) {
-            maintext.setText("Home");
-            subtext.setText("Welcome to my game.");
-            Image image = new Image("home.jpg");
-            img.setImage(image);
-            btnleft.setDisable(false);
-            count++;
-        }
         if (count == 0) {
             maintext.setText("Sewers");
             subtext.setText("The Sewer has become contaminated with toxic fungi, you will need to be vaccinated with blood from a corpse located in the Morgue in order to pass through.");
@@ -89,5 +108,17 @@ public class Controller {
             btnright.setDisable(true);
             count++;
         }
+        if (count == -1) {
+            maintext.setText("Home");
+            subtext.setText("Welcome to my game.");
+            Image image = new Image("home.jpg");
+            img.setImage(image);
+            btnleft.setDisable(false);
+            count++;
+        }
+    }
+
+    public void onBtnExit(ActionEvent actionEvent) {
+        System.exit(0);
     }
 }
